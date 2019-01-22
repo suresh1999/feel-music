421519fb7452bbac7ce6d41435ec995ce9e69a48
Suresh Kumar <mail.sk1999@gmail.com>
2019-01-23 00:49:46 +0530

    'test_3'

--- MUSIXM/settings.py
+++ MUSIXM/settings.py
@@ -130,5 +130,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
 
-STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles'),
+STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
 # django_heroku.settings(locals())
